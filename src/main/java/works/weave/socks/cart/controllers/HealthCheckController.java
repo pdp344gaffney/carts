316e493d5d6b8276b7067ffeb2671075ee5f1be8
root <root@ip-10-0-1-185.ec2.internal>
2020-07-14 18:07:11 +0000

    Modified the health controller.

--- src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
+++ src/main/java/works/weave/socks/cart/controllers/HealthCheckController.java
@@ -39,7 +39,7 @@ public class HealthCheckController {
        }
 
        healthChecks.add(app);
-       // healthChecks.add(database);
+       healthChecks.add(database);
 
        map.put("health", healthChecks);
        return map;
